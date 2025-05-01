c9a7e5ceb37ac03388d35378819a4d325574b908
AhmadDzulfikar <dzulfikar19.ahmad@gmail.com>
2025-05-01 14:45:20 +0700

    [RED] Add skeleton for Administrator model unit tests

---
+++ src/test/java/id/ac/ui/cs/advprog/authentication/model/AdministratorTest.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.authentication.model;
+
+import org.junit.jupiter.api.Test;
+
+public class AdministratorTest {
+    @Test
+    void testAdministratorGettersAndSetters() {
+    }
+
+    @Test
+    void testAdministratorToString() {
+    }
+}
