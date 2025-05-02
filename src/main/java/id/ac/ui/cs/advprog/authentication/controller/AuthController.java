90e52c624c728154e1c554cdf937fb53a50566f4
sopigoo <shofinadhifa24@gmail.com>
2025-05-02 18:15:47 +0700

    [GREEN] Implement controller

--- src/main/java/id/ac/ui/cs/advprog/authentication/controller/AuthController.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/controller/AuthController.java
@@ -15,17 +15,21 @@ public class AuthController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthResponse> register(@RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
     }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
     }
 
     @PutMapping("/change-password")
     public ResponseEntity<String> changePassword(@RequestBody UpdatePasswordRequest request) {
+        return ResponseEntity.ok(authService.changePassword(request));
     }
 
     @PostMapping("/logout")
     public ResponseEntity<String> logout() {
+        return ResponseEntity.ok("Logout successful.");
     }
 }
