1bfd97f772b6a0ea8a0ccf78c1ce3e9f10f4a0ce
sopigoo <shofinadhifa24@gmail.com>
2025-05-02 17:35:21 +0700

    [RED] Add user dto skeletons

---
+++ src/main/java/id/ac/ui/cs/advprog/authentication/dto/AuthResponse.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.authentication.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AuthResponse {
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/authentication/dto/RegisterRequest.java
@@ -0,0 +1,8 @@
+package id.ac.ui.cs.advprog.authentication.dto;
+
+import id.ac.ui.cs.advprog.authentication.enums.Role;
+import lombok.Data;
+
+@Data
+public class RegisterRequest {
+}
