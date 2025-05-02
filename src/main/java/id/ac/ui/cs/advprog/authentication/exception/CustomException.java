bed3c1ded08cf772bbab420de6e336bb70631f96
sopigoo <shofinadhifa24@gmail.com>
2025-05-02 17:41:59 +0700

    [GREEN] Implements custom exception

--- src/main/java/id/ac/ui/cs/advprog/authentication/exception/CustomException.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/exception/CustomException.java
@@ -2,5 +2,6 @@ package id.ac.ui.cs.advprog.authentication.exception;
 
 public class CustomException extends RuntimeException {
     public CustomException(String message) {
+        super(message);
     }
 }
