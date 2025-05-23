dba4ef966ec511a4b232476f0d45de7609e7f668
sopigoo <shofinadhifa24@gmail.com>
2025-05-23 10:27:53 +0700

    [GREEN] Fix update password request implementation

--- src/main/java/id/ac/ui/cs/advprog/authentication/dto/UpdatePasswordRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/authentication/dto/UpdatePasswordRequest.java
@@ -1,10 +1,23 @@
 package id.ac.ui.cs.advprog.authentication.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpdatePasswordRequest {
+    @NotBlank(message = "Email cannot be blank")
     private String email;
+
+    @NotBlank(message = "Old password cannot be blank")
     private String oldPassword;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]+$",
+            message = "Password must contain at least one uppercase letter, one number, and one special character"
+    )
     private String newPassword;
 }
