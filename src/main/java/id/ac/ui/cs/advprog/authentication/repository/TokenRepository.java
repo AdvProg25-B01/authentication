1dd33e99b512ba89519f4b320522b1ab110986c8
sopigoo <shofinadhifa24@gmail.com>
2025-05-23 09:10:48 +0700

    [RED] Add skeleton for token repository

---
+++ src/main/java/id/ac/ui/cs/advprog/authentication/repository/TokenRepository.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.advprog.authentication.repository;
+
+import id.ac.ui.cs.advprog.authentication.model.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token, Long> {
+
+}
